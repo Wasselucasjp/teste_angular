0fddc66b15c9a56216c556c14ecc6b53a544cf97
Wasse Lucas <wasselucas.silva@gmail.com>
2023-11-03 11:25:58 -0300

    rotas e importacoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './produtos/components/header/header.component';
+import { HomeComponent } from './produtos/page/home/home.component';
+import { CadastrarProdutoComponent } from './produtos/page/cadastrar-produto/cadastrar-produto.component';
+import { ProdutoComponent } from './produtos/page/produto/produto.component';
+import { EditarProdutoComponent } from './produtos/page/editar-produto/editar-produto.component';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CadastrarProdutoComponent,
+    ProdutoComponent,
+    EditarProdutoComponent,
     HeaderComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CurrencyMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/interfaces/produto.ts
+++
@@ -1,6 +0,0 @@
-export interface IProduto {
-    id:number
-    nomeProduto:string
-    precoProduto:number
-    codigoDeBarra:string
-}
